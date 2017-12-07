4bb891085f373a34456d012f44c3254a411952ba
mkambol <mkambol@gmail.com>
2017-12-07 10:45:43 -0500

    [BACKLOG-17271] First pass at a generic api and base impl for streaming
    This commit includes the StreamSource and StreamWindow interfaces, which
    define contracts for receiving data and processing it within windowed
    chunks.

---
+++ engine/src/main/java/org/pentaho/di/trans/streaming/common/BaseStreamStep.java
@@ -0,0 +1,113 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2017 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.di.trans.streaming.common;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
+import org.pentaho.di.core.Result;
+import org.pentaho.di.core.RowMetaAndData;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettleStepException;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.trans.step.BaseStep;
+import org.pentaho.di.trans.step.StepDataInterface;
+import org.pentaho.di.trans.step.StepMeta;
+import org.pentaho.di.trans.step.StepMetaInterface;
+import org.pentaho.di.trans.step.StepStatus;
+import org.pentaho.di.trans.streaming.api.StreamSource;
+import org.pentaho.di.trans.streaming.api.StreamWindow;
+
+import java.util.Collection;
+import java.util.List;
+
+public class BaseStreamStep<I> extends BaseStep {
+
+  private final StreamWindow<I, Result> window;
+  private final StreamSource<I> source;
+
+  public BaseStreamStep( StepMeta stepMeta,
+                         StepDataInterface stepDataInterface, int copyNr,
+                         TransMeta transMeta, Trans trans,
+                         StreamWindow<I, Result> window,
+                         StreamSource<I> source ) {
+    super( stepMeta, stepDataInterface, copyNr, transMeta, trans );
+    this.window = window;
+    this.source = source;
+  }
+
+
+  public boolean processRow( StepMetaInterface smi, StepDataInterface sdi ) throws KettleException {
+
+    Preconditions.checkArgument( first,
+      "Streaming steps should only have processRow called once." );
+
+    Iterable<Result> subTransResults = window.buffer( source.rows() );
+
+    subTransResults.forEach( result -> {
+        if ( result.getNrErrors() > 0 ) {
+          stopAll();
+        } else {
+          putRows( result.getRows() );
+        }
+      }
+    );
+    return false;
+  }
+
+
+  @Override
+  public void stopRunning( StepMetaInterface stepMetaInterface, StepDataInterface stepDataInterface )
+    throws KettleException {
+    source.close();
+    super.stopRunning( stepMetaInterface, stepDataInterface );
+  }
+
+  @Override public void resumeRunning() {
+    source.resume();
+    super.resumeRunning();
+  }
+
+  @Override public Collection<StepStatus> subStatuses() {
+    //return kafkaConsumerInputData.subtransExecutor.getStatuses().values();
+    // TODO, handle statuses.
+    return null;
+  }
+
+  @Override public void pauseRunning() {
+    source.pause();
+    super.pauseRunning();
+  }
+
+  private void putRows( List<RowMetaAndData> rows ) {
+    rows.forEach( row -> {
+      try {
+        putRow( row.getRowMeta(), row.getData() );
+      } catch ( KettleStepException e ) {
+        Throwables.propagate( e );
+      }
+    } );
+  }
+
+
+}
